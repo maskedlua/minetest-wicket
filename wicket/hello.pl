d28e3d1283a2cc7d7cb1149d028efa0c0458db07
Xiong Changnian <xiong@cpan.org>
2013-12-24 22:01:50 -0800

    Add some boilerplate.
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

---
+++ wicket/hello.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+#       hello.pl
+#       = Copyright 2014 Xiong Changnian  <xiong@cpan.org>   =
+#       = Free Software = Artistic License 2.0 = NO WARRANTY =
+
+use 5.014002;   # 5.14.3    # 2012  # pop $arrayref, copy s///r
+use strict;
+use warnings;
+
+#~ use lib qw| lib |;
+#~ use Error::Base;
+#~ use Perl6::Form;
+#~ use Test::More;
+#~ use Test::Trap;
+
+#~ use Devel::Comments '###';
+#~ use Devel::Comments '###', ({ -file => 'debug.log' });                   #~
+
+## use
+#============================================================================#
+say "$0 Running...";
+
+say 'Hello, world!';
+
+say "Done.";
+exit;
+#----------------------------------------------------------------------------#
+
+
+#============================================================================#
+__END__     
