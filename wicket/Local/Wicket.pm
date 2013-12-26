33a5860b1a4ede39af3d35a8858f62d937142b2d
Xiong Changnian <xiong@cpan.org>
2013-12-26 06:18:20 -0800

    Create blank module Local::Wicket
    * Just not going to try to release this to CPAN.
    
    Signed-off-by: Xiong Changnian <xiong@cpan.org>

---
+++ wicket/Local/Wicket.pm
@@ -0,0 +1,171 @@
+package Local::Wicket;
+# Choose minimum perl interpreter version; delete the rest.
+# Do you want to enforce the bugfix level?
+#~ use 5.008008;   # 5.8.8     # 2006  # oldest sane version
+#~ use 5.008009;   # 5.8.9     # 2008  # latest 5.8
+#~ use 5.010001;   # 5.10.1    # 2009  # say, state, switch
+#~ use 5.012003;   # 5.12.5    # 2011  # yada
+#~ use 5.014002;   # 5.14.3    # 2012  # pop $arrayref, copy s///r
+#~ use 5.016002;   # 5.16.2    # 2012  # __SUB__
+use strict;
+use warnings;
+use version; our $VERSION = qv('v0.0.0');
+
+# Core modules
+
+# CPAN modules
+
+# Alternate uses
+#~ use Devel::Comments '###', ({ -file => 'debug.log' });                   #~
+
+## use
+#============================================================================#
+
+# Pseudo-globals
+
+## pseudo-globals
+#----------------------------------------------------------------------------#
+
+
+
+## END MODULE
+1;
+#============================================================================#
+__END__
+
+=head1 NAME
+
+Local::Wicket - .................. 44 chars in PAUSE upload!
+
+=head1 VERSION
+
+This document describes Local::Wicket version v0.0.0
+
+=head1 SYNOPSIS
+
+    use Local::Wicket;
+
+=head1 DESCRIPTION
+
+=over
+
+I<< Anyone can tell the truth,  >> 
+I<< but only very few of us can make epigrams. >> 
+-- W. Somerset Maugham
+
+=back
+
+=head1 METHODS 
+
+=head2 new()
+
+=head1 ACCSESSORS
+
+Object-oriented accessor methods are provided for each parameter and result. 
+They all do just what you'd expect. 
+
+    $self               = $self->put_attr($string);
+    $string             = $self->get_attr();
+
+=head1 SEE ALSO
+
+L<< Some::Module|Some::Module >>
+
+=head1 INSTALLATION
+
+This module is installed using L<< Module::Build|Module::Build >>. 
+
+=head1 DIAGNOSTICS
+
+=over
+
+=item C<< some error message >>
+
+Some explanation. 
+
+=back
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+None. 
+
+=head1 DEPENDENCIES
+
+There are no non-core dependencies. 
+
+=begin html
+
+<!--
+
+=end html
+
+L<< version|version >> 0.99 E<10> E<8> E<9>
+Perl extension for Version Objects
+
+=begin html
+
+-->
+
+<DL>
+
+<DT>    <a href="http://search.cpan.org/perldoc?version" 
+            class="podlinkpod">version</a> 0.99 
+<DD>    Perl extension for Version Objects
+
+</DL>
+
+=end html
+
+This module should work with any version of perl 5.8.8 and up. 
+However, you may need to upgrade some core modules. 
+
+=head1 INCOMPATIBILITIES
+
+None known.
+
+=head1 BUGS AND LIMITATIONS
+
+This is an early release. Reports and suggestions will be warmly welcomed. 
+
+Please report any issues to: 
+L<https://github.com/Xiong/devel-toolbox/issues>.
+
+=head1 DEVELOPMENT
+
+This project is hosted on GitHub at: 
+L<https://github.com/Xiong/devel-toolbox>. 
+
+=head1 THANKS
+
+Somebody helped!
+
+=head1 AUTHOR
+
+Xiong Changnian C<< <xiong@cpan.org> >>
+
+=head1 LICENSE
+
+Copyright (C) 2013 
+Xiong Changnian C<< <xiong@cpan.org> >>
+
+This library and its contents are released under Artistic License 2.0:
+
+L<http://www.opensource.org/licenses/artistic-license-2.0.php>
+
+=begin fool_pod_coverage
+
+No, I'm not just lazy. I think it's counterproductive to give each accessor 
+its very own section. Sorry if you disagree. 
+
+=head2 put_attr
+
+=head2 get_attr
+
+=end   fool_pod_coverage
+
+=cut
+
+
+
+
+
